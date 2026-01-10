ecc2c95c5c813ce242250cd04136c9eebdddfb63
Jansley <jciciliato@gmail.com>
2026-01-10 18:07:20 -0300

    Ajustes e melhorias III - 10/01/2026

--- backend/services/supabase_client.py
+++ backend/services/supabase_client.py
@@ -2,9 +2,9 @@ import os
 from supabase import create_client, Client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 if not SUPABASE_URL or not SUPABASE_KEY:
-    raise RuntimeError("SUPABASE_URL ou SUPABASE_SERVICE_ROLE_KEY não definidos")
+    raise RuntimeError("SUPABASE_URL ou SUPABASE_KEY não definidos")
